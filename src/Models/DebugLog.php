53db5577fc44d02cf7adace396d72ad621584aca
Galtsev Timofey <galtsev.t@yandex.ru>
2026-05-24 23:18:49 +0700

    docs: add README for demo app

--- src/Models/DebugLog.php
+++ src/Models/DebugLog.php
@@ -5,10 +5,14 @@ namespace App\Models;
 use App\Debug\QueryCollector;
 use App\Shared\Behaviors\CastBehavior;
 use App\Shared\Caster\Casts\JsonCast;
+use Yii1x\ActiveRecord\Attributes\Database;
+use Yii1x\ActiveRecord\Attributes\Table;
 use Yiisoft\Router\Debug\RouterCollector;
 use Yiisoft\Yii\Debug\Collector\Web\RequestCollector;
 use Yiisoft\Yii\Debug\Collector\Web\WebAppInfoCollector;
 
+#[Database('db_main')]
+#[Table('debug_log')]
 class DebugLog extends BaseModel
 {
     public function behaviors(): array
@@ -21,11 +25,6 @@ class DebugLog extends BaseModel
                 ],
             ],
         ];
-    }
-
-    public function tableName(): string
-    {
-        return 'debug_log';
     }
 
     public function jsonSerialize(): array
